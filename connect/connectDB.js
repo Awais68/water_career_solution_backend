960ec489cf3be9b0868f616fc7a2e8879bb32e7b
Awais68 <hamza768@gmail.com>
2025-05-14 20:11:59 +0500

    MongoDB Connected

---
+++ connect/connectDB.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import "dotenv/config";
+
+export const connectDB = () => {
+  const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+  console.log("process.env==>>>", DB_USER, DB_PASSWORD);
+
+  const url = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@clusterawais.cnuqrfr.mongodb.net/${DB_NAME}`;
+  try {
+    mongoose.connect(url);
+    console.log("Connected Successfully...");
+  } catch (error) {
+    console.log(error);
+  }
+};
--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-
 import express from "express";
+import { connectDB } from "./connect/connectDB.js";
 
 const app = express();
-
+connectDB();
 app.get("/", (req, res) => {
-    res.send("Hellow there !!! Server Is Running")
-})
+  res.send("Hellow there !!! Server Is Running");
+});
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000........")
-})
-
-
-
-
+  console.log("Server is running on port 3000........");
+});
