085f36f8be89007a9ffe929a4612fd62e87f1678
Awais68 <hamza768@gmail.com>
2025-05-16 18:54:30 +0500

    Route Defined

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { connectDB } from "./connect/connectDB.js";
+import userRouter from "./routes/userRoute.js";
 
 const app = express();
 connectDB();
@@ -7,7 +8,11 @@ app.get("/", (req, res) => {
   res.send("Hellow there !!! Server Is Running");
 });
 
+app.use(express.json());
+app.use(cors());
+
+app.use("/user", userRouter);
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000........");
 });
-
---
+++ routes/userRoute.js
@@ -0,0 +1,11 @@
+import express from "express";
+import UserModel from "../modules/userSchema";
+
+const userRouter = express.Router();
+
+userRouter.post("/signup", (req, res) => {
+    
+})
+
+
+export default userRouter;
